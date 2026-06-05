0e8e20e980f655f255290266a5f73dd8e7c619fb
okei@keio.jp <glriefaet@icloud.com>
2026-06-05 23:02:38 +0900

    add jobs properties and simulator pages

--- app.js
+++ app.js
@@ -18,3 +18,26 @@ function calculate() {
     "result"
   ).innerText = `月収:${income}NZD 貯金:${savings}NZD`;
 }
+const startBtn = document.getElementById("startBtn");
+
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    window.location.href = "jobs.html";
+  });
+}
+
+function calculate() {
+  const wage = Number(document.getElementById("hourlyWage").value);
+
+  const hours = Number(document.getElementById("hours").value);
+
+  const rent = Number(document.getElementById("rent").value);
+
+  const income = wage * hours * 4;
+
+  const savings = income - rent * 4;
+
+  document.getElementById(
+    "result"
+  ).innerText = `月収: ${income} NZD ／ 推定貯金: ${savings} NZD`;
+}
